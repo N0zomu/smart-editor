e74e821af49e6725a35d3ded071e8be06a53c50a
李扬 <21373135>
2024-05-26 22:55:59 +0800

    login, register and home

---
+++ src/api/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+const serverAddress = "http://152.136.110.235:8000";
+
+export function post(url, data) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(serverAddress + url, data)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        // ElNotification({
+        //   message: "网络连接失败",
+        //   type: "error",
+        //   showClose: true,
+        //   position: "top-right",
+        //   duration: 2000,
+        // });
+      });
+  });
+}
+export function get(url, param) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(serverAddress + url, {
+        params: param,
+      })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        // ElNotification({
+        //   message: "网络连接失败",
+        //   type: "error",
+        //   showClose: true,
+        //   position: "top-right",
+        //   duration: 2000,
+        // });
+      });
+  });
+}
--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import App from './App.vue'
 import router from './router'
 import './assets/css/global.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import axios from "axios";
 
 
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
